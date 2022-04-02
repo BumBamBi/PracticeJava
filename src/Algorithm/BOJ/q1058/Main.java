ca816d1ed6e508e7b33b887b7c55a01bfd96e523
BumBamBi <dlrhkddn0325@gmail.com>
2022-04-02 19:51:33 +0900

    modify

---
+++ src/Algorithm/BOJ/q1058/Main.java
@@ -0,0 +1,28 @@
+package Algorithm.BOJ.q1058;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    static int N;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+
+        ArrayList<Integer> list = new ArrayList<Integer>()[10];
+
+        
+
+        for (int i = 0; i < N; i++) {
+            String s = sc.nextLine();
+            for (int j = 0; j < s.length(); j++) {
+                if (s.charAt(j) == 'N') {
+                    
+                }
+            }
+        }
+
+        sc.close();
+    }
+}
--- src/Algorithm/BOJ/q1450_yet/Main.java
+++ src/Algorithm/BOJ/q1450_yet/Main.java
@@ -1,4 +1,4 @@
-package Algorithm.BOJ.q1450;
+package Algorithm.BOJ.q1450_yet;
 
 import java.util.Scanner;
 
@@ -18,19 +18,12 @@ public class Main {
         int[][] dp = new int[N+1][C+1];
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= C; j++) {
-                if (dp[i - 1][j - weights[i]] + weights[i] <= j) {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - weights[i]] + weights[i]);
+                if (weights[i] <= j) {
+                    dp[i][j] = Math.max(dp[i - 1][j] + weights[i], dp[i - 1][j - weights[i]]);
                 } else {
                     dp[i][j] = dp[i - 1][j];
                 }
             }
-        }
-
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= C; j++) {
-                System.out.print(dp[i][j]);
-            }
-            System.out.println("");
         }
 
         System.out.println(dp[N][C]);
