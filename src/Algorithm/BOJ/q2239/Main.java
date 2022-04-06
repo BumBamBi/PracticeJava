9729045648af2aa17db02cc1d8af38b95fa52652
BumBamBi <dlrhkddn0325@gmail.com>
2022-04-06 15:42:10 +0900

    mod 1058

--- src/Algorithm/BOJ/q2239/Main.java
+++ src/Algorithm/BOJ/q2239/Main.java
@@ -1,13 +1,13 @@
-package Algorithm.BOJ.q1058_yet;
+package Algorithm.BOJ.q2239;
 
 import java.util.Scanner;
 
 public class Main {
-    static int N;
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        N = sc.nextInt();
+        
 
         sc.close();
     }
