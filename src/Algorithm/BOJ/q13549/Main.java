9e26d6235e3edd0d59121eaf508d6c9661fe947a
BumBamBi <dlrhkddn0325@gmail.com>
2022-08-22 23:22:36 +0900

    [mod] 13549 : 알고리즘 수정

--- src/Algorithm/BOJ/q13549/Main.java
+++ src/Algorithm/BOJ/q13549/Main.java
@@ -26,36 +26,13 @@ public class Main {
     }
     
     public static void func(int cur, int cnt) {
-        multi(cur, cnt);
         
-        for (int i = 0; i <= 100000; i++) {
-            if (flags[i] != -1) {
-                plus(i + 1, cnt + 1);
-                multi(i, cnt + 1);
-            }
-        }
+        // 곱하기
+        // 더하기 - 곱하기
+        // 빼기 - 곱하기
     }
     
     public static void multi(int input, int cnt) {
-        int temp = input;
-
-        while (temp > 0 && temp <= 100000) {
-            if (flags[temp] > cnt) {
-                flags[temp] = cnt;
-            }
-            temp += input;
-        }
-    }
-
-    public static void plus(int input, int cnt) {
-        if (flags[input+1] > cnt) {
-            flags[input+1] = cnt;
-        }
-    }
-
-    public static void minus(int input, int cnt) {
-        if (flags[input-1] > cnt) {
-            flags[input-1] = cnt;
-        }
+        
     }
 }
