12f76a550d9124d3c162e31471c5bd0f6b54f286
BumBamBi <dlrhkddn0325@gmail.com>
2022-08-24 23:47:20 +0900

    [add] 1026 : solving

--- src/Algorithm/BOJ/q1026/Main.java
+++
@@ -1,7 +0,0 @@
-package Algorithm.BOJ.q1026;
-
-public class Main {
-    public static void main(String[] args) {
-        
-    }
-}
---
+++ src/Algorithm/BOJ/q1026_yet/Main.java
@@ -0,0 +1,13 @@
+package Algorithm.BOJ.q1026_yet;
+
+import java.util.Scanner;
+
+public class Main {
+    static int N;
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        sc.close();
+    }
+}
