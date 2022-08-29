ec85d6dee1bf2d9a19bae38e5e61bd1cde4a95a8
BumBamBi <dlrhkddn0325@gmail.com>
2022-08-29 23:56:04 +0900

    [MOD] 1026 : add input param

--- src/Algorithm/BOJ/q1026_yet/Main.java
+++ src/Algorithm/BOJ/q1026_yet/Main.java
@@ -4,11 +4,26 @@ import java.util.Scanner;
 
 public class Main {
     static int N;
+    static int[] A;
+    static int[] B;
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // git test 1
+        N = sc.nextInt();
+
+        A = new int[N+1];
+        B = new int[N+1];
+
+        for (int i = 0; i < N; i++) {
+            A[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < N; i++) {
+            B[i] = sc.nextInt();
+        }
+
+        
 
         sc.close();
     }
