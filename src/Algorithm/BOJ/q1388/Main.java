dc01a239e39a703a6ee4960da2513e01bac52d76
BumBamBi <dlrhkddn0325@gmail.com>
2022-08-31 01:18:49 +0900

    [ADD] 1388 : solve

--- src/Algorithm/BOJ/q1388/Main.java
+++ src/Algorithm/BOJ/q1388/Main.java
@@ -7,6 +7,8 @@ public class Main {
     static int N, M;
     static char[][] arr;
     static boolean[][] visited;
+
+    static int[] dxy = { -1, 1 };
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -23,21 +25,54 @@ public class Main {
             }
         }
 
+
+        int ans = 0;
+
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
                 if (visited[i][j] == false) {
-                    func(i, j);
+                    func(i, j, arr[i][j]);
+                    ans++;
                 }
             }
         }
+        
+        System.out.println(ans);
 
         sc.close();
     }
-    public static void func(int row, int col) {
+
+    public static void func(int row, int col, char curChar) {
         visited[row][col] = true;
 
-        for (int i = 0; i < 4; i++) {
-            
+        if (curChar == '|') {
+            for (int i = 0; i < 2; i++) {
+                int curRow = row + dxy[i];
+                if(isAvailable(curRow, col) == true){
+                    if (arr[curRow][col] == '|') {
+                        func(curRow, col, arr[curRow][col]);
+                    }
+                }
+            }
+        } else if (curChar == '-') {
+            for (int i = 0; i < 2; i++) {
+                int curCol = col + dxy[i];
+                if(isAvailable(row, curCol) == true){
+                    if (arr[row][curCol] == '-') {
+                        func(row, curCol, arr[row][curCol]);
+                    }
+                }
+            }
         }
     }
+
+    private static boolean isAvailable(int curRow, int curCol) {
+        if (curRow >= 0 && curCol >= 0 && curRow < N && curCol < M) {
+            if (visited[curRow][curCol] == false) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
 }
