9c198a981c7b5559c3a94f269c2b905b85b95111
BumBamBi <dlrhkddn0325@gmail.com>
2022-09-01 23:57:57 +0900

    [MOD] 1398 : add comment

--- src/Algorithm/BOJ/q1389/Main.java
+++ src/Algorithm/BOJ/q1389/Main.java
@@ -3,10 +3,16 @@ package Algorithm.BOJ.q1389;
 import java.util.Scanner;
 
 public class Main {
+    static int N, M;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        N = sc.nextInt();
+        M = sc.nextInt();
         
+        // 그래프 이론 혹은
+        // 플루이드와샬
 
         sc.close();
     }
