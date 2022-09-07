e3d27e1dd28f6527e424699802691602169a5169
BumBamBi <dlrhkddn0325@gmail.com>
2022-09-07 23:40:07 +0900

    [MOD] 1389 : add comments

--- src/Algorithm/BOJ/q1389/Main.java
+++ src/Algorithm/BOJ/q1389/Main.java
@@ -25,7 +25,7 @@ public class Main {
         for (int i = 0; i < N; i++) {
             boolean[] flag = new boolean[N]; // boolean? int?
 
-            func();
+            // 그래프 혹은 플루이드 와샬
         }
 
         sc.close();
